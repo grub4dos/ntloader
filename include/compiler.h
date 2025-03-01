72294d1203cfd9f142b2768837ce397ab9a8cabe
a1ive <10670106+a1ive@users.noreply.github.com>
2025-03-01 20:59:45 +0900

    add safemath

--- include/compiler.h
+++ include/compiler.h
@@ -45,4 +45,56 @@
 /* Mark parameter as unused */
 #define __unused __attribute__ ((unused))
 
+/* GCC version checking borrowed from glibc. */
+#if defined(__GNUC__) && defined(__GNUC_MINOR__)
+#  define GNUC_PREREQ(maj,min) \
+((__GNUC__ << 16) + __GNUC_MINOR__ >= ((maj) << 16) + (min))
+#else
+#  define GNUC_PREREQ(maj,min) 0
+#endif
+
+/* Does this compiler support compile-time error attributes? */
+#if GNUC_PREREQ(4,3)
+#  define GRUB_ATTRIBUTE_ERROR(msg) \
+__attribute__ ((__error__ (msg)))
+#else
+#  define GRUB_ATTRIBUTE_ERROR(msg) __attribute__ ((noreturn))
+#endif
+
+#if defined(__clang__) && defined(__clang_major__) && defined(__clang_minor__)
+#  define CLANG_PREREQ(maj,min) \
+((__clang_major__ > (maj)) || \
+(__clang_major__ == (maj) && __clang_minor__ >= (min)))
+#else
+#  define CLANG_PREREQ(maj,min) 0
+#endif
+
+/* Safe math */
+#if GNUC_PREREQ(5, 1) || CLANG_PREREQ(8, 0)
+
+#define safe_add(a, b, res) __builtin_add_overflow(a, b, res)
+#define safe_sub(a, b, res) __builtin_sub_overflow(a, b, res)
+#define safe_mul(a, b, res) __builtin_mul_overflow(a, b, res)
+
+#define safe_cast(a, res)   grub_add ((a), 0, (res))
+
+/*
+ * It's caller's responsibility to check "align" does not equal 0 and
+ * is power of 2.
+ */
+#define ALIGN_UP_OVF(v, align, res) \
+({ \
+    bool __failed; \
+    typeof(v) __a = ((typeof(v))(align) - 1); \
+    \
+    __failed = safe_add (v, __a, res); \
+    if (__failed == false) \
+        *(res) &= ~__a; \
+        __failed; \
+})
+
+#else
+#error gcc 5.1 or newer or clang 8.0 or newer is required
+#endif
+
 #endif /* _COMPILER_H */
